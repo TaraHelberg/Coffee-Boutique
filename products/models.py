354f6214e0fcff1a6d90a5e345ab722de5400d11
TaraHelberg <taralhelberg@outlook.com>
2023-01-26 17:50:01 +0000

    Added updated & created on & sizes to product model & size selector box to product templates

--- products/models.py
+++ products/models.py
@@ -29,8 +29,11 @@ class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True,
                                  on_delete=models.SET_NULL)
     sku = models.CharField(max_length=250, null=True, blank=True)
+    update_on = models.DateTimeField(auto_now=True)
+    created_on = models.DateTimeField(auto_now_add=True, blank=True, null=True)
     name = models.CharField(max_length=255)
     description = models.TextField()
+    has_sizes = models.BooleanField(default=False, null=True, blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
