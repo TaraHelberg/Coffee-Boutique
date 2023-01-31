956ec7955669e86220ffd0a30de8b010f2a96a35
TaraHelberg <taralhelberg@outlook.com>
2023-01-31 16:27:47 +0000

    Added profiles app,models,views,template & urls

--- checkout/models.py
+++ checkout/models.py
@@ -7,10 +7,14 @@ from django.conf import settings
 from django_countries.fields import CountryField
 
 from products.models import Product
+from profiles.models import UserProfile
 
 
 class Order(models.Model):
     order_number = models.CharField(max_length=32, null=False, editable=False)
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
+                                     null=True, blank=True,
+                                     related_name='orders')
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
@@ -69,7 +73,7 @@ class OrderLineItem(models.Model):
                               related_name='lineitems')
     product = models.ForeignKey(Product, null=False, blank=False,
                                 on_delete=models.CASCADE)
-    product_size = models.CharField(max_length=6, null=True, blank=True)  # 125 gm, 250 gm, 340 gm, 500 gm, 1 kg
+    product_size = models.CharField(max_length=6, null=True, blank=True)   # 125 gm, 250 gm, 340 gm, 500 gm, 1 kg
     quantity = models.IntegerField(null=False, blank=False, default=0)
     lineitem_total = models.DecimalField(max_digits=6, decimal_places=2,
                                          null=False, blank=False,
