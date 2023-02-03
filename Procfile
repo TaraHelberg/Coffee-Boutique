1f2b74fa248a50c80d989662e531a9c3e89121a3
TaraHelberg <taralhelberg@outlook.com>
2023-02-03 15:28:08 +0000

    Add Procfile & Heroku Deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn coffee_boutique.wsgi:application
--- coffee_boutique/settings.py
+++ coffee_boutique/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['coffee-boutique.herokuapp.com', 'localhost']
+
 
 
 # Application definition
@@ -120,12 +121,17 @@ WSGI_APPLICATION = 'coffee_boutique.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
