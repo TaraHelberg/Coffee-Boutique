feaa80f27ea72af75c2ce26d3057e3da3b972de0
TaraHelberg <taralhelberg@outlook.com>
2023-02-14 11:35:25 +0000

    Update favourites view,template & urls

--- favourites/views.py
+++ favourites/views.py
@@ -1,28 +1,9 @@
 from django.shortcuts import render
-from django.contrib import messages
-from products.models import Product
-from .models import Favourites
+
+# Create your views here.
 
 
 def favourites(request):
-    """Users Favourite Products"""
-    favourite_items_count = 0
+    """ A view to return the index page """
 
-    try:
-        all_favourites = Favourites.objects.filter(username=request.user.id)[0]
-    except IndexError:
-        favourite_items = None
-    else:
-        favourite_items = all_favourites.products.all()
-        favourite_items_count = all_favourites.products.all().count()
-
-    if not favourite_items:
-        messages.info(request, 'Your favourites list is empty!')
-
-    template = 'favourites/favourites.html'
-    context = {
-        'favourite_items': favourite_items,
-        'favourite_items_count': favourite_items_count
-    }
-
-    return render(request, template, context)
+    return render(request, 'favourites/favourites.html')
