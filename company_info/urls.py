26d7dd7a1fc9478491de2867d4c3e3026991f17d
TaraHelberg <taralhelberg@outlook.com>
2023-02-24 15:11:24 +0000

    Add add_blog view,ulrs & templates. & add urls links to base & top header

--- company_info/urls.py
+++ company_info/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.blog, name='blog'),
+    path('add_blog/', views.add_blog, name='add_blog'),
     path('contact_us/', views.contact_us, name='contact_us'),
     path('copyright/', views.copyright, name='copyright'),
     path('faq/', views.faq, name='faq'),
--- company_info/views.py
+++ company_info/views.py
@@ -2,13 +2,42 @@ from django.shortcuts import render, redirect, reverse
 from django.core.mail import send_mail
 from django.contrib import messages
 from django.conf import settings
-from .forms import ContactUsForm
+
+from .forms import ContactUsForm, BlogForm
 
 
 def blog(request):
-    """ A view to return the blog page """
+    """View Blog Posts"""
 
     return render(request, 'company_info/blog.html')
+
+
+def add_blog(request):
+    """Add Blog"""
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
+    if request.method == 'POST':
+        form = BlogForm(request.POST or None)
+        if form.is_valid():
+            blog = form.save()
+            messages.success(request, 'Successfully added Blog Post!')
+            return redirect(reverse('blog'))
+        else:
+            messages.error(request,
+                           f'Failed to add blog.'
+                           f'Please ensure the form is valid.'
+                           )
+    else:
+        form = BlogForm()
+
+    template = 'company_info/add_blog.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
 
 
 def contact_us(request, *args, **kwargs):
