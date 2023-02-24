2e3b28e80ac9ae6cd0563c3faa614d3a2394eecc
TaraHelberg <taralhelberg@outlook.com>
2023-02-24 09:30:40 +0000

    Update url path company_infor,add drop down About nav,Add comments to all & base.html

--- company_info/urls.py
+++ company_info/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.contact_us, name='contact_us'),
+    path('contact_us/', views.contact_us, name='contact_us'),
     path('copyright/', views.copyright, name='copyright'),
     path('faq/', views.faq, name='faq'),
 ]
